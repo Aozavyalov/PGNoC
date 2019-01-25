f1dfd8927d4d0af4ba3b6804f9ae78a2fa16b54f
Завьялов Андрей <aozavyalov@protonmail.com>
2019-01-25 23:36:53 +0300

    Changed flits generation. Now it generates full packet and then send flits

--- src/IP/fabric.v
+++ src/IP/fabric.v
@@ -39,83 +39,90 @@ module fabric #(
   reg [2:0] gen_state; // state reg
 
   integer generated_packs;
+  integer generated_flits;
   integer pack_len;
   integer freq_cntr;
 
   reg [ADDR_SIZE-1:0] dest_addr;
-  reg [DATA_SIZE-1:0] gen_data;
-  reg fin_flag;                  // reg about finishing
+  reg [DATA_SIZE*MAX_PACK_LEN-1:0] new_packet; // a reg for a full generated packet
+  reg fin_flag;                                // reg about finishing
 
   // generating data to send
-  always @(posedge clk, posedge a_rst)
-    case (gen_state)
-    PACK_GEN:
-    begin
-      pack_len = $urandom % MAX_PACK_LEN;
-      dest_addr = $urandom % NODES_NUM;
-      if (dest_addr != ADDR)
+  always @(posedge clk)
+    if (!a_rst)
+      case (gen_state)
+      PACK_GEN:
       begin
-        gen_state = FLIT_GEN;
-        if (DEBUG)
-          $display("%5d|%3h|new package|len: %2h|addr: %3h", time_reg, ADDR, pack_len + 1, dest_addr);
-        else
-          $fdisplay(log_file, "%5d|%3h|new package|len: %2h|addr: %3h", time_reg, ADDR, pack_len + 1, dest_addr);
-      end
-    end
-    FLIT_GEN:
-      if (r_ready_in == 1'b0)
-        if (freq_cntr == FREQ - 1)
-        begin
-          freq_cntr = 0;
-          wr_ready_out = 1'b1;
-          gen_data = $urandom;
-          data_o = {gen_data, (pack_len == 0 ? 1'b1 : 1'b0), dest_addr};  
-          if (DEBUG)     
-            $display("%5d|%3h|new flit|%b", time_reg, ADDR, data_o);
-          else
-            $fdisplay(log_file, "%5d|%3h|new flit|%b", time_reg, ADDR, data_o);
-          gen_state = FLIT_SEND;
-        end
-        else
-          freq_cntr = freq_cntr + 1;
-    FLIT_SEND:
-      if (r_ready_in == 1'b1)
-      begin
-        wr_ready_out = 1'b0;
-        if (DEBUG)
-          $display("%5d|%3h|flit sended|%b", time_reg, ADDR, data_o);
-        else
-          $fdisplay(log_file, "%5d|%3h|flit sended|%b", time_reg, ADDR, data_o);
-        if (pack_len == 0)
-        begin
-          generated_packs = generated_packs + 1;
-          if (DEBUG)
-            $display("%5d|%3h|package sended|%2d", time_reg, ADDR, generated_packs);
-          else
-            $fdisplay(log_file, "%5d|%3h|package sended|%2d", time_reg, ADDR, generated_packs);
-          if (generated_packs == PACKS_TO_GEN)
-            gen_state = GEN_FINISH;
-          else
-            gen_state = PACK_GEN;
-        end
-        else
+        pack_len = 1 + $urandom % MAX_PACK_LEN;
+        dest_addr = $urandom % NODES_NUM;
+        for (generated_flits=0; generated_flits < DATA_SIZE*pack_len/32; generated_flits = generated_flits + 1)
+          new_packet[generated_flits*32+:32] = $urandom;
+        generated_flits = 0;
+        if (dest_addr != ADDR)
         begin
           gen_state = FLIT_GEN;
-          pack_len  = pack_len - 1;
+          if (DEBUG)
+            $display("%5d|%3h|new package|len: %2h|addr: %3h", time_reg, ADDR, pack_len, dest_addr);
+          else
+            $fdisplay(log_file, "%5d|%3h|new package|len: %2h|addr: %3h", time_reg, ADDR, pack_len, dest_addr);
+        end
+        else
+          if (DEBUG)
+            $display("%5d|%3h|addr %3h is wrong. Regenerating package", time_reg, ADDR, dest_addr);
+          else
+            $fdisplay(log_file, "%5d|%3h|addr %3h is wrong. Regenerating package", time_reg, ADDR, dest_addr);
+      end
+      FLIT_GEN:
+        if (r_ready_in == 1'b0)
+          if (freq_cntr == FREQ - 1)
+          begin
+            freq_cntr = 0;
+            wr_ready_out = 1'b1;
+            data_o = {new_packet[generated_flits*DATA_SIZE+:DATA_SIZE], (pack_len - 1 == generated_flits ? 1'b1 : 1'b0), dest_addr};
+            generated_flits = generated_flits + 1;
+            if (DEBUG)     
+              $display("%5d|%3h|new flit|%b", time_reg, ADDR, data_o);
+            else
+              $fdisplay(log_file, "%5d|%3h|new flit|%b", time_reg, ADDR, data_o);
+            gen_state = FLIT_SEND;
+          end
+          else
+            freq_cntr = freq_cntr + 1;
+      FLIT_SEND:
+        if (r_ready_in == 1'b1)
+        begin
+          wr_ready_out = 1'b0;
+          if (DEBUG)
+            $display("%5d|%3h|flit sended|%b", time_reg, ADDR, data_o);
+          else
+            $fdisplay(log_file, "%5d|%3h|flit sended|%b", time_reg, ADDR, data_o);
+          if (pack_len == generated_flits)
+          begin
+            generated_packs = generated_packs + 1;
+            if (DEBUG)
+              $display("%5d|%3h|package sended|%2d", time_reg, ADDR, generated_packs);
+            else
+              $fdisplay(log_file, "%5d|%3h|package sended|%2d", time_reg, ADDR, generated_packs);
+            if (generated_packs == PACKS_TO_GEN)
+              gen_state = GEN_FINISH;
+            else
+              gen_state = PACK_GEN;
+          end
+          else
+            gen_state = FLIT_GEN;
+        end
+      GEN_FINISH:
+      begin
+        if (!fin_flag)
+        begin
+          if (DEBUG)
+            $display("%5d|%3h|finish generating|%5d", time_reg, ADDR, generated_packs);
+          else
+            $fdisplay(log_file, "%5d|%3h|finish generating|%5d", time_reg, ADDR, generated_packs);
+          fin_flag = 1'b1;
         end
       end
-    GEN_FINISH:
-    begin
-      if (!fin_flag)
-      begin
-        if (DEBUG)
-          $display("%5d|%3h|finish generating|%5d", time_reg, ADDR, generated_packs);
-        else
-          $fdisplay(log_file, "%5d|%3h|finish generating|%5d", time_reg, ADDR, generated_packs);
-        fin_flag = 1'b1;
-      end
-    end
-    default: gen_state = PACK_GEN;
+      default: gen_state = PACK_GEN;
   endcase
   //////////////////////////////////// end generating ////////////////////////////////////
 
@@ -125,48 +132,49 @@ module fabric #(
   integer recv_flits;                        // received flits num
   integer wrong_flits;                       // wrong flits num
   
-  always @(posedge clk, posedge a_rst)
-    case (recv_state)
-    GET_FLIT:
-      if (wr_ready_in == 1'b1)  // then it has flit to get
+  always @(posedge clk)
+    if (!a_rst)
+      case (recv_state)
+      GET_FLIT:
+        if (wr_ready_in == 1'b1)  // then it has flit to get
+        begin
+          r_ready_out = 1'b1;     // accept getting
+          recv_state  = WAIT_NEXT;   // next state
+          if (ADDR != data_i[ADDR_SIZE-1:0])  // if wrong address
+          begin
+            // writing message about wrong flit
+            if (DEBUG)
+              $display("%5d|%3h|recved wrong flit|real addr: %3h|%b", time_reg, ADDR, data_i[ADDR_SIZE-1:0], data_i);
+            else
+              $fdisplay(log_file, "%5d|%3h|recved wrong flit|real addr: %3h|%b", time_reg, ADDR, data_i[ADDR_SIZE-1:0], data_i);
+            wrong_flits = wrong_flits + 1;
+          end
+          else
+          begin
+            // write message about getting flit
+            if (DEBUG)
+              $display("%5d|%3h|recved flit|%2d|%b", time_reg, ADDR, recv_flits, data_i);
+            else
+              $fdisplay(log_file, "%5d|%3h|recved flit|%2d|%b", time_reg, ADDR, recv_flits, data_i);
+            recv_flits = recv_flits + 1;
+          end
+          if (data_i[ADDR_SIZE] == 1'b1)    // if last flit of a package
+          begin
+            if (DEBUG)
+              $display("%5d|%3h|recved package|packages: %2d", time_reg, ADDR, recv_packs);
+            else
+              $fdisplay(log_file, "%5d|%3h|recved package|packages: %2d", time_reg, ADDR, recv_packs);
+            recv_packs = recv_packs + 1;
+            recv_flits = 0;
+          end
+        end
+      WAIT_NEXT:
       begin
-        r_ready_out = 1'b1;     // accept getting
-        recv_state  = WAIT_NEXT;   // next state
-        if (ADDR != data_i[ADDR_SIZE-1:0])  // if wrong address
-        begin
-          // writing message about wrong flit
-          if (DEBUG)
-            $display("%5d|%3h|recved wrong flit|real addr: %3h|%b", time_reg, ADDR, data_i[ADDR_SIZE-1:0], data_i);
-          else
-            $fdisplay(log_file, "%5d|%3h|recved wrong flit|real addr: %3h|%b", time_reg, ADDR, data_i[ADDR_SIZE-1:0], data_i);
-          wrong_flits = wrong_flits + 1;
-        end
-        else
-        begin
-          // write message about getting flit
-          if (DEBUG)
-            $display("%5d|%3h|recved flit|%2d|%b", time_reg, ADDR, recv_flits, data_i);
-          else
-            $fdisplay(log_file, "%5d|%3h|recved flit|%2d|%b", time_reg, ADDR, recv_flits, data_i);
-          recv_flits = recv_flits + 1;
-        end
-        if (data_i[ADDR_SIZE] == 1'b1)    // if last flit of a package
-        begin
-          if (DEBUG)
-            $display("%5d|%3h|recved package|packages: %2d", time_reg, ADDR, recv_packs);
-          else
-            $fdisplay(log_file, "%5d|%3h|recved package|packages: %2d", time_reg, ADDR, recv_packs);
-          recv_packs = recv_packs + 1;
-          recv_flits = 0;
-        end
+        r_ready_out = 1'b0;
+        recv_state = GET_FLIT;
       end
-    WAIT_NEXT:
-    begin
-      r_ready_out = 1'b0;
-      recv_state = GET_FLIT;
-    end
-    default: recv_state = GET_FLIT;
-    endcase
+      default: recv_state = GET_FLIT;
+      endcase
 
   //////////////////////////////////// end receiving ////////////////////////////////////
 
