c420aa3ff5a94f227b4595259ccf8d916420fe94
Завьялов Андрей <aozavyalov@protonmail.com>
2019-04-11 14:31:30 +0300

    Fixed generation

--- src/connector/torus_gen.v
+++ src/connector/torus_gen.v
@@ -6,29 +6,19 @@ module torus #(
   input  [NODES_NUM*4*PORT_SIZE-1:0] data_i,
   output [NODES_NUM*4*PORT_SIZE-1:0] data_o
 );
-  genvar i, j;
+  genvar sw_src, sw_dst;
   generate
-    for ( i = 0; i < NODES_NUM; i = i + 1 ) // input index
-      for ( j = i + 1; j < NODES_NUM; j = j + 1 ) // output index
-        if ( ( j == i + 1 ) & ( j % H_SIZE != 0 ) ) // port 0 to 2
-          begin
-            assign data_o [i*4*PORT_SIZE+0*PORT_SIZE+:PORT_SIZE] = data_i [j*4*PORT_SIZE + 2*PORT_SIZE+:PORT_SIZE];
-            assign data_o [j*4*PORT_SIZE+2*PORT_SIZE+:PORT_SIZE] = data_i [i*4*PORT_SIZE + 0*PORT_SIZE+:PORT_SIZE];
-          end
-        else if ( (j == i + 2) & ( i % H_SIZE == 0) ) // port 2 to 0
-          begin
-            assign data_o [j*4*PORT_SIZE+0*PORT_SIZE+:PORT_SIZE] = data_i [i*4*PORT_SIZE + 2*PORT_SIZE+:PORT_SIZE];
-            assign data_o [i*4*PORT_SIZE+2*PORT_SIZE+:PORT_SIZE] = data_i [j*4*PORT_SIZE + 0*PORT_SIZE+:PORT_SIZE];
-          end
-        else if (j == ( H_SIZE + i )) // port 1 to 3
-          begin
-            assign data_o [i*4*PORT_SIZE+1*PORT_SIZE+:PORT_SIZE] = data_i [j*4*PORT_SIZE + 3*PORT_SIZE+:PORT_SIZE];
-            assign data_o [j*4*PORT_SIZE+3*PORT_SIZE+:PORT_SIZE] = data_i [i*4*PORT_SIZE + 1*PORT_SIZE+:PORT_SIZE];
-          end
-        else if ( (j - i) == (NODES_NUM - H_SIZE) ) // port 3 to 1
-          begin
-            assign data_o [j*4*PORT_SIZE+1*PORT_SIZE+:PORT_SIZE] = data_i [i*4*PORT_SIZE + 3*PORT_SIZE+:PORT_SIZE];
-            assign data_o [i*4*PORT_SIZE+3*PORT_SIZE+:PORT_SIZE] = data_i [j*4*PORT_SIZE + 1*PORT_SIZE+:PORT_SIZE];
-          end
+    for ( sw_src = 0; sw_src < NODES_NUM; sw_src = sw_src + 1 ) // input index
+      for ( sw_dst = 0; sw_dst < NODES_NUM; sw_dst = sw_dst + 1 ) // output index
+        if (((sw_dst == sw_src + 1) && (sw_src % H_SIZE != H_SIZE - 1)) || ((sw_src % H_SIZE == H_SIZE - 1) && (sw_src-sw_dst-H_SIZE+1 == 0)))    
+        begin
+          assign data_o [sw_src*4*PORT_SIZE+0*PORT_SIZE+:PORT_SIZE] = data_i [sw_dst*4*PORT_SIZE + 2*PORT_SIZE+:PORT_SIZE];
+          assign data_o [sw_dst*4*PORT_SIZE+2*PORT_SIZE+:PORT_SIZE] = data_i [sw_src*4*PORT_SIZE + 0*PORT_SIZE+:PORT_SIZE];
+        end
+        else if ((sw_dst == sw_src + H_SIZE) || (NODES_NUM - sw_src + sw_dst == H_SIZE))
+        begin
+          assign data_o [sw_src*4*PORT_SIZE+1*PORT_SIZE+:PORT_SIZE] = data_i [sw_dst*4*PORT_SIZE + 3*PORT_SIZE+:PORT_SIZE];
+          assign data_o [sw_dst*4*PORT_SIZE+3*PORT_SIZE+:PORT_SIZE] = data_i [sw_src*4*PORT_SIZE + 1*PORT_SIZE+:PORT_SIZE];
+        end
   endgenerate
 endmodule
